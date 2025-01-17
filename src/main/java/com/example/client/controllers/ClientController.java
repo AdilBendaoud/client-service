af588b667c8da8943cb4ceea7be1c26114756e17
AdilBendaoud <adilbendaoud@gmail.com>
2025-01-18 00:57:00 +0100

    update test

--- src/main/java/com/example/client/controllers/ClientController.java
+++ src/main/java/com/example/client/controllers/ClientController.java
@@ -3,6 +3,7 @@ package com.example.client.controllers;
 import com.example.client.entities.Client;
 import com.example.client.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,5 +28,10 @@ public class ClientController {
     @PostMapping
     public void save(@RequestBody Client client) {
         service.addClient(client);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<?> test(){
+        return ResponseEntity.ok("this is test for trigger");
     }
 }
